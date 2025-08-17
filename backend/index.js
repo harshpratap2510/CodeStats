95a78f8bccd674268aef7ef35d029b2090a739d1
harshpratap2510 <harshpratapnitp@gmail.com>
2025-08-17 19:37:35 +0530

    just a little change

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: ["http://localhost:5173","https://code-stats-rose.vercel.app/"],
+  origin: ["http://localhost:5173","https://code-stats-rose.vercel.app"],
   credentials: true,
 }));
 // app.use('/uploads', express.static(path.join(path.resolve(__dirname), 'uploads')));
