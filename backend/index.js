cd9d380f36533978d6fbc4b4121d8e6a1ba4111b
harshpratap2510 <harshpratapnitp@gmail.com>
2025-08-18 08:49:57 +0530

    trust proxy

--- backend/index.js
+++ backend/index.js
@@ -24,6 +24,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.set("trust proxy", 1);
 app.use(cors({
   origin: ["http://localhost:5173","https://code-stats-c618.vercel.app"],
   credentials: true,
